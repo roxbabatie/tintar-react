4399a1116ceeef96c0bae5ac1096adacb2763769
Roxana Babatie <roxana.babatie@gmail.com>
2016-02-23 12:45:43 +0200

    first commit, react installed

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+var AppComponent = React.createClass({
+
+  render: function () {
+    return (
+     <div>Hello</div>
+    );
+  }
+});
+
+
+export default AppComponent;
